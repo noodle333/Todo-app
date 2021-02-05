60018a535bcf4cc125bf92acdcbcca4f4b0c93e6
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-02-05 09:03:50 +0100

    first committ

---
+++ app.js
@@ -0,0 +1,8 @@
+//FUNCTIONS
+
+//DOCUMENT SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoInput = document.querySelector('.todo-input');
+
+//LISTENERS
