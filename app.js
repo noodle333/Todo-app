8230d318087d686bfd800dbb5a53e6545ec12071
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-02-05 10:50:17 +0100

    add remove and complete buttons + filter menu

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //LISTENERS
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //FUNCTIONS
 function addTodo(event){
@@ -15,7 +16,7 @@ function addTodo(event){
     todoDiv.classList.add("todo");
     //CREATE LIST
     const newTodo = document.createElement('li');
-    newTodo.innertext = 'hello';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //CHECKMARK BUTTON
@@ -26,8 +27,31 @@ function addTodo(event){
     //DELETE BUTTON
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-    deleteButton.classList.add("complete-btn"); //trash btn?
+    deleteButton.classList.add("delete-btn"); //trash btn?
     todoDiv.appendChild(deleteButton);
     //APPEND TO LIST
     todoList.appendChild(todoDiv);
+    //CLEAR INPUT VALUE
+    todoInput.value = "";
+}
+
+function deleteCheck(e){
+    //console.log(e.target);
+    const item = e.target;
+    //DELETE TODO
+    if(item.classList[0] == "delete-btn")
+    {
+        const todo = item.parentElement; //FRÅGA SEBBE OM DEHÄR E BRA ELLER OM JA BORDE GÖRA SOM HONOM
+        //REMOVE ANIMATION
+        todo.classList.add("removed");
+        todo.addEventListener("trasitionend", function(){
+            todo.remove();
+        });
+    }
+
+    //CHECK TODO
+    if(item.classList[0] === "complete-btn"){ //FRÅGA SEBBE VARFÖR MAN ANVÄNDER ===
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
